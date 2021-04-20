00695be37fd6319ee52cba8dd4b62cc714a4a768
zava810 <zinglish@kakao.com>
2021-04-20 19:32:05 +0530

    vk.com/zs810

---
+++ imez/time_tyre_trigonometry/heksataims.js
@@ -0,0 +1,31 @@
+my       very  educated    mother   just     sho      us       nain     plqnet
+mrkri    vinqs qrxh        mars     jupitr   strun    yurens   nqpcun   pluto
+pluto qb plqnet nhi mana jaxa.
+/*                desiml_kaunt desiml_taim                                  */
+"earth rotates once every 23 hours, 56 minutes and 4.09053 seconds, called the sidereal period, and its circumference is roughly 40,075 kilometers." 
+23x60x60 + 56x60 + 4.09053 = 86164.09053 (desi_seconds)
+1day = 1 sidereal period = 16(desiml_kaunt) hhours = 20(heks_kaunt) hhours
+1 circumference = 
+1 hekssecond rotation = 1 heksdegree rotation
+16x64x64(desiml_kaunt) = 65536(desiml_kaunt) heks_seconds in 16(desiml_kaunt) heks_hours
+1heks_digri = 360/(64)(desiml_kaunt) = 5.6875(desiml_kaunt)(desi_digri) = 
+86164.09053 / 65536  = 1.314759682(desiml_kaunt) seconds = 1heks_seconds
+vn_1 gigahrtj = 1314759682 (desiml_kaunt) saikils_pr_heks_sqkqnds
+vn_1 gigahrtj = 4>5=:402 (heks_kaunt) saikils_pr_heks_sqkqnds
+
+1 gigahertz is equal to 1,000(desiml_kaunt) megahertz (MHz) or 1,000,000,000(desiml_kaunt) Hz.
+1 gigahertz represents 1 billion cycles per second.
+
+OUR SMILE IS THE FIRST LANGUAGE OF COMMUNICATION WITH ANY LIVING/NON LIVING BEING ON THIS PLANET AND THE BUILDING STONE, THE FOUNDATION OF OUR MILES TO MILES, YEARS TO YEARS ALIVE HAPPY & ALIVE RELATIONSHIP.
+
+    aoqr smail is w phrst lqfgyuej oph komyunikesn
+    viwh qni livifg/non livifg biifg on wis & w bilidifg ston,
+    w phoundesqn oph aoqr mails tu mails,
+    iyqrs tu iyqrs qlaiv hppi & qlaiv rilesnship.
+    
+    
+kofgrqts inpiyo (dqniql) lii phur 2_2_tu iyrs oph vrk qt _sk_ hainiks.
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,40 @@
+# mintos
+mintos = minu tree os ( starting point : linuks mint vith unicase same height font u5c.ttf ( phrom DeliusUnicase )
+
+pliiz vieu this prozect site in **firefoks** only vith custom DeliusUnicase **[u5c.ttf/u5cdot.ttf][unicasefonts]**.
+
+This os vill be ekstension to linuks mint.
+
+in this only unicase same height font [u5c.ttf][unicasefonts] vill be uzed.
+
+> zn se zinc , zr se zirconium but no j q in mendeelev periodic table in chemistry.
+
+so z vill be in zemistry810 for more precision zemistry for medicines & other fields.
+
+&
+
+J vill be in maths as :
+
+> J = zilevn = 8 + 3 = L+1 = ten+1 = q - 1 = kvlv - 1 = w -2 = dblyu - 2 = X - 3 = Aksen - 3 = F -4 = phen - 4 = 10 - 5 = vnti - phaiv
+> 2 Thumbs phur biometric sign , shou , svitch
+> 8 phingers to type & count as :
+>> 8+8 = 10 = vnti = 4*4 
+
+![mintos keyboard](imez/zpg/kiibord_heksifglish1.jpg)
+
+> **underscore under pipe** pipes out keys uniphormly
+
+try hpop_u5.apk u5c.ttf hksuletr.apk at [zava810 github repository][zava810]
+
+above zinglish keyboard has symbols phur :
+
+1. phonics / phonetics : vovels / sopht consonants / hard consonants
+2. heks810 arithmetic numerals vith 8+8=10=vnti glyphs Az shoun in 1st green rou in keyboard.
+3. programming symbols
+4. punctuation symbols
+5. arithmetic symbols
+
+
+[zava810]: http://github.com/zava810/zava810
+
+[unicasefonts]: https://github.com/Font77/unicase_phonts
